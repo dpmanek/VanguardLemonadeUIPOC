03fff3bd5b9c391dee4300f2698dcdeaf9047a07
Deep Manek <dpmanek@gmail.com>
2024-11-25 21:21:10 -0500

    edit fixed and changed css

--- stencil.config.ts
+++ stencil.config.ts
@@ -18,6 +18,8 @@ export const config: Config = {
     {
       type: 'www',
       serviceWorker: null, // disable service workers
+      // baseUrl: '/StencilBedrockAgenticUI/',
+      // empty: false,
     },
   ],
   testing: {
